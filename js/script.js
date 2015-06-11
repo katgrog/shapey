83dea4b1b6c898df7a837b637dd8b87e7f8614f5
Sam Mason <sam.jbmason@gmail.com>
2015-06-11 15:20:01 +0100

    Add in Tweens

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // alert('hello!');
 
 var player1 = {
-	name: 'Katie', 
+	name: 'Katie',
 	score:0
 }
 
 
 var shapes =  ['square', 'circle', 'triangle'];
+
+var pressed = false;
 
 
 $(document).on('keydown', function(){
@@ -14,28 +16,30 @@ $(document).on('keydown', function(){
 	console.log('currentShape', currentShape);
 
 	// When somebody presses a key - check what key was pressed
-	console.log(event.which); 
-	if (event.which == 83) {
+	console.log(event.which);
+	if (event.which == 83 && pressed == false) {
 		console.log('you just pressed S!')
 
-		// Person has just pressed square 
+		// Person has just pressed square
 
 
-	// Once have key - check if key relates to a shape 
+	// Once have key - check if key relates to a shape
 		if ('square' == currentShape) {
+			pressed = true;
 
 			console.log('Hooray!');
 
-			player1.score = player1.score + 1; 
+			player1.score = player1.score + 1;
 
 			$('.score').text('score: ' + player1.score);
-		} 
+		}
 
-	} else if (event.which == 74) {
+	} else if (event.which == 74 && pressed == false) {
 		console.log('you just pressed J!')
 
-	// Check key against current shape 
+	// Check key against current shape
 		if ('triangle' == currentShape) {
+			pressed = true;
 
 			console.log('Well done, that\'s a triangle!')
 
@@ -45,10 +49,11 @@ $(document).on('keydown', function(){
 		}
 
 
-	} else if (event.which == 68) {
+	} else if (event.which == 68 && pressed == false) {
 		console.log('you just pressed D!')
 
 		if ('circle' == currentShape) {
+			pressed = true;
 
 			console.log('I love circles, well done!')
 
@@ -60,18 +65,16 @@ $(document).on('keydown', function(){
 
 
 
-	// If correct - score point 
-	// Create somewhere to store points  
+	// If correct - score point
+	// Create somewhere to store points
 
-	
+
 })
 
 
-$('.circle, .triangle, .square').hide();
+var currentShape = 'circle';
 
-var currentShape = 'circle'; 
-
-$('.circle').show();
+$('.shape').addClass('circle');
 
 setInterval( function(){
 
@@ -79,48 +82,48 @@ setInterval( function(){
 
 	// console.log(randomNumber);
 
-	// Hide whatever is in there 
-	$('.circle, .triangle, .square').hide();
+	// Hide whatever is in there
+	$('.shape').removeClass('circle triangle square');
 
-	// Randomly pick a new shape 
+	// Randomly pick a new shape
 	var newShape = shapes[randomNumber];
-	
-	// Updating currentShape to match the newShape  
+
+	// Updating currentShape to match the newShape
 	currentShape = newShape;
 
 	// console.log(currentShape);
 
 	// Show shape
-	$('.' + newShape).show();	
+	$('.shape').addClass(newShape);
+	pressed = false
 
 
 
-}, 1000);
+}, 2000);
 
 
 
 
 
-
-// Player: 
+// Player:
 // name and score
-// Keys: 
-// s, d, j, k - event.which to find out the number that relates to it 
+// Keys:
+// s, d, j, k - event.which to find out the number that relates to it
 
 
-// Step 1: 
+// Step 1:
 // I want the shapes to change randomly:
 
-// I want all shapes to be hidden 
-// I want shape 1 to be visible 
-// Then shape 1 to disappear after a second and shape 2 to be visible 
+// I want all shapes to be hidden
+// I want shape 1 to be visible
+// Then shape 1 to disappear after a second and shape 2 to be visible
 
 
 
-// Step 2: 
+// Step 2:
 // I want selected keys on the keyboard to score points only if they're the same as the current shape:
 
-// Store the current shape somewhere 
+// Store the current shape somewhere
 
 
 
